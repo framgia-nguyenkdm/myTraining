489a46efaf5b02eca25f396f97a6b934aec997ca
Framgia-nguyenkdm <khuc.dinh.minh.nguyen@framgia.com>
2018-04-02 14:12:27 +0700

    Add custom table view cell

---
+++ Lession3/Lession3_DynamicTableView/Lession3_DynamicTableView/CustomTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTableViewCell.swift
+//  Lession3_DynamicTableView
+//
+//  Created by khuc.d.m.nguyen on 4/2/18.
+//  Copyright © 2018 khuc.d.m.nguyen. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var personalImg: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Lession3/Lession3_DynamicTableView/Lession3_DynamicTableView/ViewController.swift
+++ Lession3/Lession3_DynamicTableView/Lession3_DynamicTableView/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myTableView: UITableView!
+    var myArr = ["Nguyen","Huy","Dao","Ngoc","Duc"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myTableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,3 +25,26 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        cell.personalImg.image = UIImage(named: "icon_person")
+        cell.nameLabel.text = myArr[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    
+}
