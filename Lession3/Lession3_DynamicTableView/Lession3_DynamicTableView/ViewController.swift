5e1d80c8ccb4daf8cb677a4f3d082a3505c1b011
Framgia-nguyenkdm <khuc.dinh.minh.nguyen@framgia.com>
2018-04-02 13:52:24 +0700

    Init Project Dynamic TableView

---
+++ Lession3/Lession3_DynamicTableView/Lession3_DynamicTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lession3_DynamicTableView
+//
+//  Created by khuc.d.m.nguyen on 4/2/18.
+//  Copyright © 2018 khuc.d.m.nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
