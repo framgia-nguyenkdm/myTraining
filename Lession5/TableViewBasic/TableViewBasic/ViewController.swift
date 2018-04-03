2001a7fef1c48496716c2272af7267756c5f23b1
Framgia-nguyenkdm <khuc.dinh.minh.nguyen@framgia.com>
2018-04-03 15:38:37 +0700

    Init TableViewBasic Project

---
+++ Lession5/TableViewBasic/TableViewBasic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewBasic
+//
+//  Created by khuc.d.m.nguyen on 4/3/18.
+//  Copyright © 2018 khuc.d.m.nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
