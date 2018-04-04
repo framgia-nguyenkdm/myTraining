734c8c8d01a0338b497441e90fcda88ed1cc4b05
Framgia-nguyenkdm <khuc.dinh.minh.nguyen@framgia.com>
2018-04-04 09:22:07 +0700

    Add basicTableView task

---
+++ Lession5/TableViewBasic/TableViewBasic/Model/City.swift
@@ -0,0 +1,19 @@
+//
+//  City.swift
+//  TableViewBasic
+//
+//  Created by khuc.d.m.nguyen on 4/4/18.
+//  Copyright © 2018 khuc.d.m.nguyen. All rights reserved.
+//
+
+import Foundation
+
+class City {
+    var name: String
+    var description: String
+    
+    init(name: String, description: String) {
+        self.name = name
+        self.description = description
+    }
+}
--- Lession5/TableViewBasic/TableViewBasic/ViewController.swift
+++ Lession5/TableViewBasic/TableViewBasic/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    var myArr = ["Ho Chi Minh", "Tokyo", "New York", "Texas", "Sentosa", "London"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = myArr[indexPath.row]
+        return cell
+    }
+}
