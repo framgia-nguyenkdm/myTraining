218afa0714c46ce7a4c68b353ece6647a5f8ead9
Framgia-nguyenkdm <khuc.dinh.minh.nguyen@framgia.com>
2018-04-04 10:46:34 +0700

    Add task for Middle project

---
+++ Lession5/TableViewMiddle/TableViewMiddle/AccessoryViewController.swift
@@ -0,0 +1,52 @@
+//
+//  AccessoryViewController.swift
+//  TableViewMiddle
+//
+//  Created by khuc.d.m.nguyen on 4/4/18.
+//  Copyright © 2018 khuc.d.m.nguyen. All rights reserved.
+//
+
+import UIKit
+
+class AccessoryViewController: UIViewController {
+    @IBOutlet weak var myTableView: UITableView!
+    var myArr = [String]()
+    var sections = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sections = ["A","B"]
+        myArr = ["Nguyen", "Huy","Duc","Tuan","Hieu","Trong","Anh","An","Ngoc","Hung"]
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension AccessoryViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.myTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = myArr[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == UITableViewCellAccessoryType.checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = UITableViewCellAccessoryType.checkmark
+        }
+    }
+}
--- Lession5/TableViewMiddle/TableViewMiddle/ViewController.swift
+++ Lession5/TableViewMiddle/TableViewMiddle/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myTableView: UITableView!
+    var myArr = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myArr = ["Nguyen", "Huy","Duc","Tuan","Hieu","Trong","Anh"]
+        
+        myTableView.setEditing(true, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+}
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.myTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = myArr[indexPath.row]
+        return cell
+    }
+    // Add delete action for Swipe
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .normal, title: "Delete") { (action, indexPath) in
+            self.myArr.remove(at: indexPath.row)
+            self.myTableView.reloadData()
+        }
+        deleteAction.backgroundColor = UIColor.red
+        return [deleteAction]
+    }
+    
+    // Moving cell
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return UITableViewCellEditingStyle.none
+    }
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        myArr.insert(myArr.remove(at: sourceIndexPath.row), at: destinationIndexPath.row)
+        print(myArr)
+        
+        self.myTableView.reloadData()
+    }
 }
 
